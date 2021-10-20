98a2b74a093686dfefddcbd687a64f8214773b10
Antlrr <81817551+MrAntlrr@users.noreply.github.com>
2021-10-20 22:08:13 +0800

    test 2
    first test worked

--- scripts/graphics/white.js
+++ scripts/graphics/white.js
@@ -1,3 +1,6 @@
 Events.on(ClientLoadEvent, b  => {
 UnitTypes.dagger.weapons.get(0).bullet.backColor = Color.valueOf("ffffff");
+UnitTypes.fortress.weapons.get(0).bullet.backColor = Color.valueOf("ffffff");
+UnitTypes.scepter.weapons.get(0).bullet.backColor = Color.valueOf("ffffff");
+UnitTypes.reign.weapons.get(0).bullet.backColor = Color.valueOf("ffffff");
 });
