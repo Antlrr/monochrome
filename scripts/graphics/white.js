43c36868641c0d1060f99d1b6554a5ee638d4f21
Antlrr <81817551+MrAntlrr@users.noreply.github.com>
2021-10-20 23:39:22 +0800

    t

--- scripts/graphics/white.js
+++ scripts/graphics/white.js
@@ -16,4 +16,8 @@ UnitTypes.antumbra.weapons.get(0).bullet.backColor = Color.valueOf("ffffff");
 UnitTypes.antumbra.weapons.get(1).bullet.backColor = Color.valueOf("ffffff");
 UnitTypes.antumbra.weapons.get(2).bullet.backColor = Color.valueOf("ffffff");
 UnitTypes.eclipse.weapons.get(2).bullet.backColor = Color.valueOf("ffffff");
+UnitTypes.retusa.weapons.get(0).bullet.backColor = Color.valueOf("ffffff");
+UnitTypes.cyerce.weapons.get(0).bullet.backColor = Color.valueOf("ffffff");
+UnitTypes.navanax.weapons.get(2).bullet.backColor = Color.valueOf("ffffff");
+UnitTypes.navanax.weapons.get(3).bullet.backColor = Color.valueOf("ffffff");
 });
