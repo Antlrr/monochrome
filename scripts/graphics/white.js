2d27a7c13c49b4788e88ed7ba6821b976d0da162
Antlrr <81817551+MrAntlrr@users.noreply.github.com>
2021-10-20 22:18:53 +0800

    Update white.js

--- scripts/graphics/white.js
+++ scripts/graphics/white.js
@@ -3,4 +3,8 @@ UnitTypes.dagger.weapons.get(0).bullet.backColor = Color.valueOf("ffffff");
 UnitTypes.fortress.weapons.get(0).bullet.backColor = Color.valueOf("ffffff");
 UnitTypes.scepter.weapons.get(0).bullet.backColor = Color.valueOf("ffffff");
 UnitTypes.reign.weapons.get(0).bullet.backColor = Color.valueOf("ffffff");
+UnitTypes.poly.weapons.get(0).bullet.backColor = Color.valueOf("ffffff");
+UnitTypes.mega.weapons.get(0).bullet.backColor = Color.valueOf("ffffff");
+UnitTypes.mega.weapons.get(1).bullet.backColor = Color.valueOf("ffffff");
+UnitTypes.quad.weapons.get(0).bullet.backColor = Color.valueOf("ffffff");
 });
