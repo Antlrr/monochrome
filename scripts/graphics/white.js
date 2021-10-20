21afe3a9513a62fa20b72f52e8d434629eed9c72
Antlrr <81817551+MrAntlrr@users.noreply.github.com>
2021-10-20 22:27:46 +0800

    front for mega

--- scripts/graphics/white.js
+++ scripts/graphics/white.js
@@ -6,5 +6,7 @@ UnitTypes.reign.weapons.get(0).bullet.backColor = Color.valueOf("ffffff");
 UnitTypes.poly.weapons.get(0).bullet.backColor = Color.valueOf("ffffff");
 UnitTypes.mega.weapons.get(0).bullet.backColor = Color.valueOf("ffffff");
 UnitTypes.mega.weapons.get(1).bullet.backColor = Color.valueOf("ffffff");
+UnitTypes.mega.weapons.get(0).bullet.frontColor = Color.valueOf("ffffff");
+UnitTypes.mega.weapons.get(1).bullet.frontColor = Color.valueOf("ffffff");
 UnitTypes.quad.weapons.get(0).bullet.backColor = Color.valueOf("ffffff");
 });
